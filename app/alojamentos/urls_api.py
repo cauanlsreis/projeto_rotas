bbff36e589075a3cf3c96a7a2543ecbe3eb2d547
cauanlsreis <cauanlucas.reis@gmail.com>
2025-05-25 16:58:49 -0300

    Separando API's get e post das tabelas alojamentos e funcionarios

--- app/alojamentos/urls_api.py
+++ app/alojamentos/urls_api.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import AlojamentosListCreateView
+from .views import AlojamentosCreateView
+from .views import AlojamentosListView
 
 urlpatterns = [
-    path('', AlojamentosListCreateView.as_view(),name='alojamento-list-create'),
+    path('', AlojamentosCreateView.as_view(),name='alojamento-create'),
+    path('visualizar', AlojamentosListView.as_view(),name='alojamento-list-view'),
 ]
--- app/alojamentos/views.py
+++ app/alojamentos/views.py
@@ -2,7 +2,10 @@ from rest_framework import generics
 from .models import Alojamentos
 from .serializers import AlojamentosSerializer
 
-class AlojamentosListCreateView(generics.ListCreateAPIView):
+class AlojamentosCreateView(generics.CreateAPIView):
     queryset = Alojamentos.objects.all()
     serializer_class = AlojamentosSerializer
 
+class AlojamentosListView(generics.ListAPIView):
+    queryset = Alojamentos.objects.all()
+    serializer_class = AlojamentosSerializer
